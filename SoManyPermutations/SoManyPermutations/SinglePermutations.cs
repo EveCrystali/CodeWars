6e02247456a511537188823eb2a6a508cfb8b14f
RTX4070TI <evequeenrevenge@gmail.com>
2025-01-02 16:52:08 +0100

    1

--- SoManyPermutations/SoManyPermutations/SinglePermutations.cs
+++ SoManyPermutations/SoManyPermutations/SinglePermutations.cs
@@ -4,33 +4,44 @@ public class Permutations
 {
     public static List<string> SinglePermutations(string input)
     {
+        // Il y a n! permutations
+
         if (input.Length == 1) return [input];
 
-        List<string> preAnswer = [];
+        HashSet<string> preAnswer = [];
 
-        foreach(char c in input)
+        for (int i = 0; i < input.Length - 1; i++)
         {
-            preAnswer = [.. preAnswer, .. MoveSpeciedCharAlongTheStringInput(c, input)];
+            string inputWithoutIChar = RemoveCharAtIndex(i, input);
+
+            HashSet<string> linearPermutation = [.. LinearPermutationAuto(i, inputWithoutIChar)];
+
+            foreach (string s in linearPermutation)
+            {
+                preAnswer = [.. preAnswer, .. SinglePermutations(s)];
+            }
         }
-        return preAnswer;
+        Console.WriteLine(preAnswer.ToString());
+        return [.. preAnswer];
     }
 
-    private static List<string> MoveSpeciedCharAlongTheStringInput(char c, string input)
+    private static List<string> LinearPermutationAuto(int index, string input)
     {
         List<string> strings = [];
-        for(int i = 0 ; i < input.Length ; i++)
+        for (int i = 0; i < input.Length; i++)
         {
-            strings.Add(InsertChar(i, c, input));
+            strings.Add(InsertChar(i, input[index], RemoveCharAtIndex(index, input)));
         }
         return strings;
     }
+
 
     private static string InsertChar(int index, char c, string input)
     {
         return string.Concat(input.AsSpan(0, index).ToString(), c, input.AsSpan(index).ToString());
     }
 
-    private static string RemoveCharAtSpecifiedIndex(int index, string input)
+    private static string RemoveCharAtIndex(int index, string input)
     {
         return string.Concat(input.AsSpan(0, index), input.AsSpan(index + 1));
     }
