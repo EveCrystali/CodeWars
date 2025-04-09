a805c1d1f8c9a90c4af4c44fd92a9fbd3ab9c1b6
RTX4070TI <evequeenrevenge@gmail.com>
2025-04-09 12:32:42 +0200

    refactor(Matrix): WIP! update Determinant method implementation
    - implement recursivity
    - implement deletion of i row and j column for minor calculation

--- MatrixDeterminant/MatrixDeterminant/Matrix.cs
+++ MatrixDeterminant/MatrixDeterminant/Matrix.cs
@@ -1,19 +1,28 @@
 public class Matrix
 {
-   public static int Determinant(int[][] matrix)
-   {
-       while (matrix[0].Length > 2)
-       {
-           for (int i = 0; i < matrix.Length; i++)
-           {
+    public static int Determinant(int[][] matrix)
+    {
+        while (matrix[0].Length > 2)
+        {
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    // Deleting row i
+                    int[][] matrix_new = matrix.Where((x, index) => index != i).ToArray();
 
-           }
-       }
-       if (matrix.Length == 2)
-       {
-           return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
-       }
+                    // Deleting column j
+                    matrix_new = matrix_new.Select(x => x.Where((y, index) => index != j).ToArray()).ToArray();
 
-       return 0;
-   }
+                    Determinant(matrix_new);
+                }
+            }
+        }
+        if (matrix.Length == 2)
+        {
+            return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+        }
+
+        return 0;
+    }
 }
