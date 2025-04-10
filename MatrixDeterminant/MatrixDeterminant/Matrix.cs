8a8f40dee6cc4d202a61cbd6e6b0724362c82562
RTX4070TI <evequeenrevenge@gmail.com>
2025-04-10 12:10:27 +0200

    fix: kata solved!

--- MatrixDeterminant/MatrixDeterminant/Matrix.cs
+++ MatrixDeterminant/MatrixDeterminant/Matrix.cs
@@ -1,3 +1,7 @@
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
 public class Matrix
 {
     public static int Determinant(int[][] matrix)
@@ -12,7 +16,11 @@ public class Matrix
         {
             return matrix[0][0];
         }
-        
+        if (matrix.Length == 2)
+        {
+            return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+        }
+
         for (int j = 0; j < matrix[0].Length; j++)
         {
             // Deleting row 0
@@ -22,11 +30,6 @@ public class Matrix
             matrix_new = matrix_new.Select(x => x.Where((y, index) => index != j).ToArray()).ToArray();
 
             result += (int)Math.Pow(-1, j) * matrix[0][j] * Determinant(matrix_new);
-        }
-
-        if (matrix.Length == 2)
-        {
-            return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
         }
 
         return result;
