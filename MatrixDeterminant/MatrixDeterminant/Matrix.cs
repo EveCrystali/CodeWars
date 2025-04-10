55fd81ae91d74f8f570c3caca2801c30ccd135dc
RTX4070TI <evequeenrevenge@gmail.com>
2025-04-10 12:06:49 +0200

    feat(Matrix): finalize Determinant method and remove unused code

--- MatrixDeterminant/MatrixDeterminant/Matrix.cs
+++ MatrixDeterminant/MatrixDeterminant/Matrix.cs
@@ -2,27 +2,33 @@ public class Matrix
 {
     public static int Determinant(int[][] matrix)
     {
-        while (matrix[0].Length > 2)
+        int result = 0;
+
+        if (matrix.Length == 0)
         {
-            for (int i = 0; i < matrix.Length; i++)
-            {
-                for (int j = 0; j < matrix[i].Length; j++)
-                {
-                    // Deleting row i
-                    int[][] matrix_new = matrix.Where((x, index) => index != i).ToArray();
-
-                    // Deleting column j
-                    matrix_new = matrix_new.Select(x => x.Where((y, index) => index != j).ToArray()).ToArray();
-
-                    Determinant(matrix_new);
-                }
-            }
+            return 0;
         }
+        if (matrix.Length == 1)
+        {
+            return matrix[0][0];
+        }
+        
+        for (int j = 0; j < matrix[0].Length; j++)
+        {
+            // Deleting row 0
+            int[][] matrix_new = matrix.Where((x, index) => index != 0).ToArray();
+
+            // Deleting column j
+            matrix_new = matrix_new.Select(x => x.Where((y, index) => index != j).ToArray()).ToArray();
+
+            result += (int)Math.Pow(-1, j) * matrix[0][j] * Determinant(matrix_new);
+        }
+
         if (matrix.Length == 2)
         {
             return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
         }
 
-        return 0;
+        return result;
     }
 }
