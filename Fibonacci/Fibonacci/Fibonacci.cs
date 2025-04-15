80ab0e65679941a618766d7e532dd9f5b1c6f0b9
RTX4070TI <evequeenrevenge@gmail.com>
2025-04-15 10:39:11 +0200

    feat(Fibonacci): implement fibonacci serie
    - handle all cases
    - BUT not performant solution
    
    NEXT : improve performance to handle the millionth in Fibonacci's recurrence quickly

--- Fibonacci/Fibonacci/Fibonacci.cs
+++ Fibonacci/Fibonacci/Fibonacci.cs
@@ -6,6 +6,23 @@ public class Fibonacci
 
     public static BigInteger fib(int n)
     {
+        if (n == 0)
+            return BigInteger.Zero;
+
+        if (n == 1)
+            return BigInteger.One;
+
+        if (n > 1)
+            return fib(n - 1) + fib(n - 2);
+
+        if (n == -1)
+            return BigInteger.One;
+
+        if (n < -1)
+        {
+            return fib(n + 2) - fib(n + 1);
+        }
+
         return BigInteger.Zero;
     }
 }
