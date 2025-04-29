dcd4e084c5d53c437d14fa1bf6335f256bc60387
RTX4070TI <evequeenrevenge@gmail.com>
2025-04-29 16:53:57 +0200

    refactor(spinWords.ts): simplify logic and remove unnecessary variables in spinWords function

--- StopgninnipSMysdroW!/src/spinWords.ts
+++ StopgninnipSMysdroW!/src/spinWords.ts
@@ -1,34 +1,20 @@
 export function spinWords(words: string): string {
 
-    let response: string = "";
+    if (words.length < 4) return words;
 
-    if (words.length < 4) {
-        return words;
-    }
-
-    let arrayWords: string[] = words.split(' ');
-
+    const arrayWords: string[] = words.split(' ');
     for (let i = 0; i < arrayWords.length;  i++) {
         const word: string = arrayWords[i];
 
-        if (word.length >= 5) {
-
+        if (word.length >= 5) 
+        {
             let wordTemp = '';
-            for (let j = 0; j <= word.length; j++) {
-                wordTemp += word.charAt(word.length - j);
+            for (let j = word.length - 1; j >= 0; j--) {
+                wordTemp += word[j];
             }
-            
             arrayWords[i] = wordTemp;
         }
-        if (i == arrayWords.length -1)
-        {
-            response += arrayWords[i];
-        }
-        else
-        {
-            response += arrayWords[i] + ' ';
-        }
     }
-
-    return response;
+    return arrayWords.join(' ');
 }
+
