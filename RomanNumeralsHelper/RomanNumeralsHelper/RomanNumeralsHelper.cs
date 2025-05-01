7800253679412af33905570b875731300043ac4a
EveCrystali <evequeenrevenge@gmail.com>
2025-05-01 12:13:59 +0200

    feat(RomanNumerals): WIP start implementation of ToRoman conversion
    introduce the initial structure and helper components for converting integers to Roman numerals.
    
    - add `VarStep` class to manage state during the conversion process.
    - add `addToResponseLetter` method to handle appending specific Roman symbols (e.g., M, C, X).
    
    Note: The main conversion logic in `ToRoman` is still under development.

--- RomanNumeralsHelper/RomanNumeralsHelper/RomanNumeralsHelper.cs
+++ RomanNumeralsHelper/RomanNumeralsHelper/RomanNumeralsHelper.cs
@@ -2,16 +2,88 @@ public class RomanNumerals
 {
     public static string ToRoman(int n)
     {
-        if (n == 0) return "";
 
 
+        if (n == 0 || n > 4000) return "";
 
+        VarStep varStep = new VarStep();
 
-        return "";
+        varStep.response = "";
+        varStep.n = n;
+        varStep.div = 1;
+        varStep.rest = 0;
+        varStep.q = 0;
+
+        do
+        {
+            if (n >= 1000)
+            {
+                varStep.C = 'M';
+                addToResponseLetter(varStep);
+
+            }
+            else if (1000 < n && n >= 500)
+            {
+
+            }
+            else if (n < 500 && n >= 100)
+            {
+
+            }
+            else if (n < 100 && n >= 50)
+            {
+
+            }
+            else if (n < 50 && n >= 10)
+            {
+
+            }
+            else if (n < 10 && n >= 5)
+            {
+
+            }
+            else
+            {
+
+                while (varStep.q >= 0)
+                {
+                    varStep.response += "I";
+                }
+            }
+        }
+        while (varStep.rest >= 1);
+
+        return varStep.response;
+    }
+
+    public class VarStep
+    {
+        public string response { get; set; } = "";
+        public int n { get; set; }
+        public int div { get; set; }
+        public int q { get; set; }
+        public int rest { get; set; }
+        public char C { get; set; }
+
+    }
+
+    public static string addToResponseLetter(VarStep varStep)
+    {
+        varStep.rest = varStep.n % varStep.div;
+        varStep.q = varStep.n / varStep.div;
+        varStep.n = varStep.q * varStep.div - varStep.rest;
+
+        while (varStep.q >= 0)
+        {
+            varStep.response += varStep.C;
+        }
+        return varStep.response;
     }
 
     public static int FromRoman(string romanNumeral)
     {
         return 0;
     }
+
+
 }
