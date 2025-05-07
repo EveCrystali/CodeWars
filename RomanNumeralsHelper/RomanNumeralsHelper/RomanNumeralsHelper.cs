61179f45cdef75fc9f78e0db7e7799962cdb9aaf
RTX4070TI <evequeenrevenge@gmail.com>
2025-05-07 15:50:31 +0200

    feat(ToRoman): handle non-subtractive cases
    - simplify ToRoman logic and improve variable usage
    - support basic additive forms like VI, XI, etc
    - add two readonly and static list Div and C

--- RomanNumeralsHelper/RomanNumeralsHelper/RomanNumeralsHelper.cs
+++ RomanNumeralsHelper/RomanNumeralsHelper/RomanNumeralsHelper.cs
@@ -1,60 +1,42 @@
+using System.Dynamic;
+
 public class RomanNumerals
 {
     public static string ToRoman(int n)
     {
-
-
         if (n == 0 || n > 4000) return "";
 
-        VarStep varStep = new VarStep();
+        VarStep varStep = new() { response = "", n = n, div = Div[0], q = 0, c = C[0] };
 
-        varStep.response = "";
-        varStep.n = n;
-        varStep.div = 1;
-        varStep.rest = 0;
-        varStep.q = 0;
+        int i = 0;
 
         do
         {
-            if (n >= 1000)
-            {
-                varStep.C = 'M';
-                addToResponseLetter(varStep);
-
-            }
-            else if (1000 < n && n >= 500)
-            {
-
-            }
-            else if (n < 500 && n >= 100)
-            {
-
-            }
-            else if (n < 100 && n >= 50)
-            {
-
-            }
-            else if (n < 50 && n >= 10)
-            {
-
-            }
-            else if (n < 10 && n >= 5)
-            {
-
-            }
-            else
-            {
-
-                while (varStep.q >= 0)
-                {
-                    varStep.response += "I";
-                }
-            }
+            addToResponseLetter(varStep);
+            if (i + 1 >= Div.Count) break;
+            varStep.div = Div[i + 1];
+            varStep.c = C[i + 1];
+            i++;
         }
-        while (varStep.rest >= 1);
+        while (varStep.n != 0);
 
         return varStep.response;
+
     }
+
+    public static void addToResponseLetter(VarStep varStep)
+    {
+        varStep.q = varStep.n / varStep.div;
+        varStep.n %= varStep.div;
+        while (varStep.q > 0)
+        {
+            varStep.response += varStep.c;
+            varStep.q--;
+        }
+    }
+
+    private static readonly List<int> Div = new List<int> { 1000, 500, 100, 50, 10, 5, 1 };
+    private static readonly List<char> C = new List<char> { 'M', 'D', 'C', 'L', 'X', 'V', 'I' };
 
     public class VarStep
     {
@@ -62,22 +44,7 @@ public class RomanNumerals
         public int n { get; set; }
         public int div { get; set; }
         public int q { get; set; }
-        public int rest { get; set; }
-        public char C { get; set; }
-
-    }
-
-    public static string addToResponseLetter(VarStep varStep)
-    {
-        varStep.rest = varStep.n % varStep.div;
-        varStep.q = varStep.n / varStep.div;
-        varStep.n = varStep.q * varStep.div - varStep.rest;
-
-        while (varStep.q >= 0)
-        {
-            varStep.response += varStep.C;
-        }
-        return varStep.response;
+        public char c { get; set; }
     }
 
     public static int FromRoman(string romanNumeral)
