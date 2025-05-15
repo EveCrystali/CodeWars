7df2a095b790ca33ece0133381bb053fd3789e81
RTX4070TI <evequeenrevenge@gmail.com>
2025-05-15 16:20:14 +0200

    refactor(RomanNumerals): seperate in two class RomanNumerals and its helpers inRomanNumeralConverter

--- RomanNumeralsHelper/RomanNumeralsHelper/RomanNumeralsHelper.cs
+++ RomanNumeralsHelper/RomanNumeralsHelper/RomanNumeralsHelper.cs
@@ -2,26 +2,27 @@ using System.Dynamic;
 using System.Numerics;
 using System.Collections.Generic;
 
-public class RomanNumerals
+public static class RomanNumerals
+{
+    public static string ToRoman(int n) => RomanNumeralConverter.ToRoman(n);
+    public static int FromRoman(string romanNumeral) => RomanNumeralConverter.FromRoman(romanNumeral);
+}
+
+public static class RomanNumeralConverter
 {
 
     private static readonly List<int> Div = new List<int> { 1000, 500, 100, 50, 10, 5, 1 };
     private static readonly List<char> C = new List<char> { 'M', 'D', 'C', 'L', 'X', 'V', 'I' };
 
-    public class ConversionToRomanState
-    {
-        public string Response { get; set; } = "";
-        public int N { get; set; }
-        public int Div { get; set; }
-        public int Q { get; set; }
-        public char C { get; set; }
-    }
+    private const int MAX_ROMAN_VALUE = 4000;
+    private const int SUBTRACTIVE_NOTATION_FOUR = 4;
+    private const int SUBTRACTIVE_NOTATION_NINE = 9;
 
     public static string ToRoman(int n)
     {
-        if (n == 0 || n > 4000) return "";
+        if (n == 0 || n > MAX_ROMAN_VALUE) return "";
 
-        ConversionToRomanState state = new() { Response = "", N = n, Div = Div[0], Q = 0, C = C[0] };
+        ConversionToRomanState state = new() { Response = "", N = n, Divisors = Div[0], Quotient = 0, Char = C[0] };
 
         do
         {
@@ -30,53 +31,67 @@ public class RomanNumerals
         while (state.N != 0);
 
         return state.Response;
-
     }
 
-    public static void AddCharByChar(ConversionToRomanState state)
+    public class ConversionToRomanState
     {
-        state.Q = state.N / state.Div;
-
-        int currentIndex = Div.IndexOf(state.Div);
-
-        if (currentIndex + 1 < Div.Count)
-        {
-            int nextDiv = Div[currentIndex + 1];
-            int tempq = state.N / nextDiv;
-
-            if (tempq == 9 || tempq == 4)
-            {
-                state.Response += C[currentIndex + 1].ToString() + C[currentIndex - 1].ToString();
-                state.N -= tempq * nextDiv;
-                return;
-            }
-        }
-
-        while (state.Q > 0)
-        {
-            state.Response += state.C;
-            state.N -= state.Div;
-            state.Q--;
-        }
-
-        if (currentIndex + 1 < Div.Count)
-        {
-            state.Div = Div[currentIndex + 1];
-            state.C = C[currentIndex + 1];
-        }
+        public string Response { get; set; } = "";
+        public int N { get; set; }
+        public int Divisors { get; set; }
+        public int Quotient { get; set; }
+        public char Char { get; set; }
     }
 
     public static int FromRoman(string romanNumeral)
     {
         int response = 0;
-
         for (int i = 0; i < romanNumeral.Length; i++)
         {
             char c1 = romanNumeral[i];
             int indexC1 = C.IndexOf(c1);
             response += Div[indexC1];
         }
-
         return response;
+    }
+
+    public static void AddCharByChar(ConversionToRomanState state)
+    {
+        state.Quotient = state.N / state.Divisors;
+        int currentIndex = Div.IndexOf(state.Divisors);
+
+        if (currentIndex + 1 < Div.Count)
+        {
+            int nextDiv = Div[currentIndex + 1];
+            int tempQuotient = state.N / nextDiv;
+
+            if (TryHandleSubtractiveNotation(state, currentIndex, nextDiv, tempQuotient))
+            {
+                return;
+            }
+        }
+
+        while (state.Quotient > 0)
+        {
+            state.Response += state.Char;
+            state.N -= state.Divisors;
+            state.Quotient--;
+        }
+
+        if (currentIndex + 1 < Div.Count)
+        {
+            state.Divisors = Div[currentIndex + 1];
+            state.Char = C[currentIndex + 1];
+        }
+    }
+
+    private static bool TryHandleSubtractiveNotation(ConversionToRomanState state, int currentIndex, int nextDiv, int tempQuotient)
+    {
+        if (tempQuotient == SUBTRACTIVE_NOTATION_NINE || tempQuotient == SUBTRACTIVE_NOTATION_FOUR)
+        {
+            state.Response += C[currentIndex + 1].ToString() + C[currentIndex - 1].ToString();
+            state.N -= tempQuotient * nextDiv;
+            return true;
+        }
+        return false;
     }
 }
