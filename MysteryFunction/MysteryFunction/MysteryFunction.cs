13b28c66d44b28b252ffad417427d17758d1835b
RTX4070TI <evequeenrevenge@gmail.com>
2025-08-28 10:41:40 +0200

    feat(MysteryFunction): add project files for the new Kata MysteryFunction and its unit tests

---
+++ MysteryFunction/MysteryFunction/MysteryFunction.cs
@@ -0,0 +1,18 @@
+using System;
+public class MysteryFunction
+{
+   public static long Mystery(long n)
+   {
+       return -1;
+   }
+
+   public static long MysteryInv(long n)
+   {
+       return -1;
+   }
+
+   public static string NameOfMystery()
+   {
+       return "";
+   }
+}
