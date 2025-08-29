91606b650b5125316e66bc2915abb21cc32c600e
RTX4070TI <evequeenrevenge@gmail.com>
2025-08-29 13:13:12 +0200

    refactor(MysteryFunction): simplify Mystery and MysteryInv methods for improved readability

--- MysteryFunction/MysteryFunction/MysteryFunction.cs
+++ MysteryFunction/MysteryFunction/MysteryFunction.cs
@@ -4,55 +4,16 @@ namespace Kata;
 
 public static class MysteryFunction
 {
-    public static long Mystery(long n)
-    {
-        long resultat = n;
-
-        for (int i = 63; i > 0; i--)
-        {
-            long masque = 1L << i;
-
-            if ((n & masque) != 0)
-            {
-                long masque2 = 1L << i - 1;
-
-                if ((n & masque2) != 0)
-                {
-                    resultat &= ~masque2;
-                }
-                else
-                {
-                    resultat |= masque2;
-                }
-            }
-        }
-
-        return resultat;
-    }
+    public static long Mystery(long n) => n ^ (n >> 1);
 
     public static long MysteryInv(long n)
     {
-        long resultat = n;
-
-        for (int i = 63; i > 0 ; i--)
+        if (n == 0)
         {
-            long masque = 1L << i;
-
-            if ((resultat & masque) != 0)
-            {
-                long masque2 = 1L << i - 1;
-
-                if ((n & masque2) != 0)
-                {
-                    resultat &= ~masque2;
-                }
-                else
-                {
-                    resultat |= masque2;
-                }
-            }
+            return 0;
         }
-        return resultat;
+
+        return n ^ MysteryInv(n >> 1);
     }
 
     public static string NameOfMystery() => "gray code";
